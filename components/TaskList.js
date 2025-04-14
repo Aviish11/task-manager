cef4d248e010a6642fae69d2e6e075b4268c97a8
Nasli-Ayoub <nasli.ayoub@hotmail.com>
2025-04-14 13:49:56 -0400

    added basics for tasklist components

--- components/TaskList.js
+++ components/TaskList.js
@@ -1,2 +1,11 @@
+import React, { useState } from 'react';
 
+export default function TaskList() {
+    const [tasks, setTasks] = useState([]);
+    const [newTitle, setNewTitle] = useState('');
+    const [newDescription, setNewDescription] = useState('');
+    const [editingIndex, setEditingIndex] = useState(null);
+
+  
+}
   
