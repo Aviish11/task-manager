458fb099b418758464e6a03ba14383c4dd4ff1a2
Sam-Ola-24 <toye.akinola@gmail.com>
2025-04-21 17:53:50 -0300

    Update contact.js
    Added a few changes for the page

--- pages/contact.js
+++ pages/contact.js
@@ -8,25 +8,31 @@ const Contact = () => {
     <div>
       <Navbar />
       <main>
-        <>
+      <div className="about">
         <h1>Contact Us</h1>
-        <p>Feel free to reach out to us using the form below!</p>
+        <p>Feel free to reach out to us using the form below!</p><br></br>
           
-          <form>
-            <>
+          <form class="contact-form">
+            <div class="form-group">
               <label htmlFor="name">Name:</label>
               <input type="text" id="name" />
-
+            </div>
+            <div class="form-group">
               <label htmlFor="email">Email:</label>
               <input type="email" id="email" />
+              </div>
 
+              <div class="form-group">
               <label htmlFor="message">Message:</label>
               <textarea id="message"></textarea>
+              </div>
 
-              <button type="submit" style={{ marginTop: '10px' }}>Send</button>
-            </>
+              
+              <button className="send-btn" type="submit" style={{ marginTop: '10px' }}>Send</button>
+           
           </form>
-        </>
+          <br></br>
+        </div>
       </main>
       <Footer />
     </div>
