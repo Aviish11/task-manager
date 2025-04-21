356466c67fc8aec0f8d060301e0d970a7620644e
Sam-Ola-24 <toye.akinola@gmail.com>
2025-04-21 17:55:26 -0300

    Update about.js
    Updated page display

--- pages/about.js
+++ pages/about.js
@@ -8,11 +8,13 @@ const About = () => {
     <div>
       <Navbar />
       <main style={{ padding: '20px' }}>
+        <div className="about">
         <h1>About Task Manager</h1>
         <p>This app was created to help users manage their daily tasks efficiently.</p>
         <p>Built using Next.js and the Todoist API.</p>
         <p>Built by Veevek Auckloo, Naslihan Mohammed Ayoub and Oluwaseun Akinola</p>
         <p></p>
+        </div>
       </main>
       <Footer />
     </div>
